df0245d9a37a9df378d9bd03b9c7fcd248dd636b
csingleton4avc <csingleton@rhilgames.com>
2025-03-06 20:00:24 -0800

    Added Comments

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 def main():
-    
+    # get user input
     maleStudents = int(input("Enter the number of male students: "))
     femaleStudents = int(input("Enter the number of female students: "))
     nonbinaryStudents = int(input("Enter the number of nonbinary students: "))
     totalStudents = maleStudents + femaleStudents + nonbinaryStudents
     
+    # calculate percentage
     m_percentage = (maleStudents / totalStudents) * 100
     f_percentage = (femaleStudents / totalStudents) * 100
     nb_percentage = (nonbinaryStudents / totalStudents) * 100
     
+    # print results
     print(f"The total number of students is: \t{totalStudents}")
     print(f"The number of males, females and non-binary: \t{maleStudents} \t{femaleStudents} \t{nonbinaryStudents}")
     print(f"The percentage of males, females and non-binary: \t{m_percentage:.2f}% \t{f_percentage:.2f}% \t{nb_percentage:.2f}%")
