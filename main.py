d1e732683133b5653e21af8ac33992f2c622bfc3
csingleton4avc <csingleton@rhilgames.com>
2025-03-06 18:49:04 -0800

    Defined input variables

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    
+    maleStudents = int(input("Enter the number of male students: "))
+    femaleStudents = int(input("Enter the number of female students: "))
+    nonbinaryStudents = int(input("Enter the number of nonbinary students: "))
+    
