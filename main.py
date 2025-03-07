6ddf30daeb0fdf86da684b6537e71275c9491c0e
csingleton4avc <csingleton@rhilgames.com>
2025-03-06 18:56:25 -0800

    Percentage of Students added

--- main.py
+++ main.py
@@ -3,4 +3,15 @@ def main():
     maleStudents = int(input("Enter the number of male students: "))
     femaleStudents = int(input("Enter the number of female students: "))
     nonbinaryStudents = int(input("Enter the number of nonbinary students: "))
+    totalStudents = maleStudents + femaleStudents + nonbinaryStudents
     
+    m_percentage = (maleStudents / totalStudents) * 100
+    f_percentage = (femaleStudents / totalStudents) * 100
+    nb_percentage = (nonbinaryStudents / totalStudents) * 100
+    
+    
+    return maleStudents, femaleStudents, nonbinaryStudents
+    
+    
+if __name__ == "__main__":
+    main()
